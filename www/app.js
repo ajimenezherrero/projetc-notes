adc5770d4a7baddb23df6afce7176a4ea8db5291
ajimenezherrero <ajimenezherrero@gmail.com>
2015-04-26 21:22:22 +0200

    Commit del post de user, no es operativo

--- www/app.js
+++ www/app.js
@@ -3,11 +3,30 @@ var app = angular.module('projectNotes',[]);
 
 app.controller('userController', ['$http', function ($http){
 	var projectNotes = this;
+	var user = {};
 	projectNotes.users = [];
 	
 	$http.get('http://localhost:8080/notas-api/user').success(function (data) {
 		projectNotes.users = data;
-    });	
+    }).
+	  error(function(data) {
+		  window.alert("ERROR");
+	});	
+	
+	this.addUser = function pushData(){
+		projectNotes.users.push(this.user);
+		$http({
+            method: 'POST',
+            url: "http://localhost:8080/notas-api/user",
+            data: this.user,
+            headers: {'Content-Type': 'application/vnd.note.api.user+json'}
+        }).success(function(data) {
+		}).error(function(data) {
+			window.alert("ERROR");
+		});
+		this.user = {};
+	};
+	
 }]);
 
 app.controller('TabController', function(){
